18d99de825467d5efb2f0adce42a4606b0c1884f
miyamae <yu1ro.mya@gmail.com>
2019-11-29 17:56:41 +0900

    typeorm 途中

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,27 @@ import { HttpModule, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from './config/config.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+const username = process.env.POSTGRES_USER || 'postgres';
+const password = process.env.POSTGRES_PASSWORD || 'example';
 
 @Module({
-  imports: [ConfigModule, HttpModule],
+  imports: [
+    UsersModule,
+    ConfigModule,
+    HttpModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username,
+      password,
+      database: 'postgres',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
